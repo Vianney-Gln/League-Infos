342b15b0669dd01fb2067bf0a99b59251499c1c5
Vianney <vgeloen@gmail.com>
2025-07-19 18:06:38 +0200

    LOL-TECH-2 Nettoyage ligne non utilisee

--- Back/src/main/java/com/league/league_infos/common/constants/ErrorMessagesEnum.java
+++ Back/src/main/java/com/league/league_infos/common/constants/ErrorMessagesEnum.java
@@ -4,9 +4,7 @@ public enum ErrorMessagesEnum {
     ERROR_BUSINESS_1("ERROR_BUSINESS_1", "Aucun invocateur ne correspond à votre recherche"),
     ERROR_BUSINESS_2("ERROR_BUSINESS_2", "Erreur lors de l'appel à l'API Riot"),
     ERROR_BUSINESS_3("ERROR_BUSINESS_3", "API Riot inaccessible pour le moment"),
-    ERROR_BUSINESS_4("ERROR_BUSINESS_4", "Le champion le plus bannis n'a pas pu être récupéré."),
-    ERROR_BUSINESS_5("ERROR_BUSINESS_5", "Accés interdit: Clé api manquante ou invalide, ou origine de la requête interdite");
-
+    ERROR_BUSINESS_4("ERROR_BUSINESS_4", "Le champion le plus bannis n'a pas pu être récupéré.");
 
     private final String libelle;
     private final String code;

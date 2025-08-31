8b91e82cd1251e222085f8c10a0f81772f2f475e
Vianney <vgeloen@gmail.com>
2025-08-31 15:43:23 +0200

    Adaptation fuseau horaire pour tu front

--- Front/src/test.ts
+++ Front/src/test.ts
@@ -2,4 +2,5 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
+(process.env as any).TZ = 'Europe/Paris';
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());

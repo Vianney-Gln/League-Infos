c7a8cd5929769292a5926580eda8f1f5bc8dcb61
Vianney <vgeloen@gmail.com>
2026-07-25 20:46:37 +0200

    [fix] rotation des champions

--- Back/src/main/java/com/league/league_infos/services/riot/RiotChampionsService.java
+++ Back/src/main/java/com/league/league_infos/services/riot/RiotChampionsService.java
@@ -1,13 +1,17 @@
 package com.league.league_infos.services.riot;
 
 import com.league.league_infos.common.constants.ApiRiotUrls;
+import com.league.league_infos.common.exceptions.BusinessException;
 import com.league.league_infos.dto.FreeChampionsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import static com.league.league_infos.common.constants.ErrorMessagesEnum.ERROR_BUSINESS_4;
 
 @Service
 public class RiotChampionsService implements com.league.league_infos.services.api.ChampionsService {
@@ -27,6 +31,12 @@ public class RiotChampionsService implements com.league.league_infos.services.ap
                 null,
                 FreeChampionsDTO.class
         );
+
+        riotResponse.getBody().setFreeChampionIds(null);
+
+        if (riotResponse.getBody() != null && (riotResponse.getBody().getFreeChampionIds() == null || riotResponse.getBody().getFreeChampionIdsForNewPlayers() == null)) {
+            throw new BusinessException(ERROR_BUSINESS_4.getLibelle(), HttpStatus.BAD_GATEWAY);
+        }
         return riotResponse.getBody();
     }
 }
